4f6145f4ce366319123d46cd516a0c097dc6e366
ekinohito <ekinohito@yandex.ru>
2025-12-01 04:10:06 +0300

    lab3 start

---
+++ lab3/lab3.cpp
@@ -0,0 +1,14 @@
+﻿// main.cpp
+#include <iostream>
+#include <omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
+        std::cout << "Hello from thread " << thread_id 
+                  << " of " << num_threads << std::endl;
+    }
+    return 0;
+}
